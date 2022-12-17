49c624642b8443be13d54f30642e6730ad169af5
SausagemanYT <Yousifalnaqar@gmail.com>
2022-12-17 13:05:14 +1000

    added alot of diffrent files and folders and a template_block and template_item the template_block is not working for some reason and currently i am trying to change createrobotics to create_robotics to make it more clear to us the use

--- src/main/java/net/Super2019/createrobotics/CreateRobotics.java
+++ src/main/java/net/Super2019/createrobotics/CreateRobotics.java
@@ -1,21 +1,15 @@
 package net.Super2019.createrobotics;
 
 import com.mojang.logging.LogUtils;
-import net.minecraft.world.level.block.Block;
+import net.Super2019.createrobotics.block.ModBlocks;
+import net.Super2019.createrobotics.item.ModItems;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.InterModComms;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
-import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
-
-import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(CreateRobotics.MOD_ID)
@@ -28,7 +22,12 @@ public class CreateRobotics {
 
     public CreateRobotics() {
         // Register the setup method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        eventBus.addListener(this::setup);
+
+        ModItems.register(eventBus);
+        ModBlocks.register(eventBus);
+
 
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
