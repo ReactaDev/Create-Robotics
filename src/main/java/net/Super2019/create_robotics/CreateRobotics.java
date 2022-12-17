5b6fba9d14dcdd604d9142ccf2900f3c6624cbf7
SausagemanYT <Yousifalnaqar@gmail.com>
2022-12-17 13:42:57 +1000

    changed MOD_ID from createrobotics to create_robotics and added both template_block and template_item in misc tab will create a creative tab later on for the stuff no textures added cause i cant rlly make textures look good thats a job for the art team

---
+++ src/main/java/net/Super2019/create_robotics/CreateRobotics.java
@@ -0,0 +1,42 @@
+package net.Super2019.create_robotics;
+
+import com.mojang.logging.LogUtils;
+import net.Super2019.create_robotics.block.ModBlocks;
+import net.Super2019.create_robotics.item.ModItems;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(CreateRobotics.MOD_ID)
+public class CreateRobotics {
+    public static final String MOD_ID = "create_robotics";
+
+    // Directly reference a slf4j logger
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+
+    public CreateRobotics() {
+        // Register the setup method for modloading
+        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        eventBus.addListener(this::setup);
+
+        ModItems.register(eventBus);
+        ModBlocks.register(eventBus);
+
+
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+        // some preinit code
+        LOGGER.info("HELLO FROM PREINIT");
+        LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+}
+
---
+++ src/main/java/net/Super2019/create_robotics/item/ModItems.java
@@ -0,0 +1,23 @@
+package net.Super2019.create_robotics.item;
+
+import net.Super2019.create_robotics.CreateRobotics;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, CreateRobotics.MOD_ID);
+
+    public static final RegistryObject<Item> TEMPLATE_ITEM = ITEMS.register("template_item",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
